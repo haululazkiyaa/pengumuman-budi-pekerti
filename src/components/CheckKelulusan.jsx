cca5ffecb203c8991358ecc6a7395992b4f4287c
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2024-11-18 23:36:09 +0700

    fix something

--- src/components/CheckKelulusan.jsx
+++ src/components/CheckKelulusan.jsx
@@ -107,7 +107,9 @@ const CheckKelulusan = () => {
   const renderCountdown = () => {
     if (timeLeft > 0) {
       const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-      const hours = Math.floor(timeLeft / (1000 * 60 * 60));
+      const hours = Math.floor(
+        (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
       const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
